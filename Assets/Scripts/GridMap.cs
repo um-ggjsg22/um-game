f08e6f78f72aa2552f0d1968d6cd4a6b415adeb3
djtfoo <djt.foo@gmail.com>
2022-01-22 01:50:29 +0800

    GridPosition

--- Assets/Scripts/GridMap.cs
+++ Assets/Scripts/GridMap.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class GridMap : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private int gridLength = 13;
+    public int GridLength
     {
-        
+        get { return gridLength; }
+    }
+
+    [SerializeField]
+    private GameObject floor;
+
+    private float gridSize; // in pixels
+    private Vector3 startPos;   // grid 0,0
+
+    [SerializeField]
+    private Transform startTest;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        // Calculate info about the grids
+        gridSize = floor.GetComponent<RectTransform>().sizeDelta.x / gridLength;
+        startPos = new Vector3(floor.transform.position.x - gridLength * 0.5f * gridSize + 0.5f * gridSize, floor.transform.position.y - gridLength * 0.5f * gridSize + 0.5f * gridSize);
+
+        // Spawn smth at start pos
+        startTest.position = startPos;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void SetPosition()
+    {
+        //startTest.position = startPos + new Vector3(startX * gridSize, startY * gridSize);
     }
 }
---
+++ Assets/Scripts/Runner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Runner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            startY = Mathf.Min(gridLength - 1, startY + 1);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            startY = Mathf.Max(0, startY - 1);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            startX = Mathf.Max(0, startX - 1);
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            startX = Mathf.Min(gridLength - 1, startX + 1);
+        }
+
+        UpdatePosition();*/
+    }
+}
